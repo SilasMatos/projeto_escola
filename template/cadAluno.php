0e17ced87b310c0e1cbb096491fa96cd6daf3098
Ícaro <icaro.ecomp@gmail.com>
2022-11-16 19:47:20 -0300

    Alt

--- template/cadAluno.php
+++ template/cadAluno.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-xl-12 col-lg-7">
+    <div class="col-xl-12 col-lg-12">
         <div class="card shadow mb-4">
             <div
                 class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -16,6 +16,8 @@
                     <label for="inputPassword4" class="form-label">Sexo</label>
                     <select class="form-control">
                         <option value="">-</option>
+                        <option value="M">Masculino</option>
+                        <option value="F">Feminino</option>
                     </select>
                 </div>
 
@@ -25,27 +27,27 @@
                 </div>
                 <div class="col-md-4">
                     <label for="inputPassword4" class="form-label">CEP</label>
-                    <input type="text" name="cep" class="form-control" id="inputEmail4">
+                    <input type="text" name="cep" id="cep"  onblur="pesquisacep(this.value);"   class="form-control">
                 </div>
 
                 <div class="col-md-12">
                     <label for="inputPassword4" class="form-label">Rua</label>
-                    <input type="text" name="rua" readonly class="form-control" id="inputEmail4">
+                    <input type="text" name="rua" id="rua" readonly class="form-control">
                 </div>
 
                 <div class="col-md-6">
                     <label for="inputPassword4" class="form-label">Cidade</label>
-                    <input type="text" name="cidade" readonly class="form-control" id="inputEmail4">
+                    <input type="text" name="cidade" id="cidade" readonly class="form-control">
                 </div>
 
                 <div class="col-md-6">
                     <label for="inputPassword4" class="form-label">Bairro</label>
-                    <input type="text" name="bairro" readonly class="form-control" id="inputEmail4">
+                    <input type="text" name="bairro" readonly class="form-control" id="bairro">
                 </div>
 
                 <div class="col-md-12">
                     <label for="inputPassword4" class="form-label">Estado</label>
-                    <input type="text" name="estado" readonly class="form-control" id="inputEmail4">
+                    <input type="text" name="estado" readonly class="form-control" id="uf">
                 </div>
 
                 <div class="col-md-12">
